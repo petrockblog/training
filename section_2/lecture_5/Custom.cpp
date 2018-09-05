2048895a0dececcba948af44de0e297dc6cfab10
Florian Müller <contact@petrockblock.com>
2018-09-05 09:48:03 +0200

    Added final code from lecture 5

--- section_2/lecture_5/Custom.cpp
+++ section_2/lecture_5/Custom.cpp
@@ -2,10 +2,28 @@
 #include <exception>
 using namespace std;
 
-class MyException: public exception {
+class MyException : public exception {
+ public:
+  virtual const char *what() const noexcept {
+    return "Exception here: Something bad happened";
+  };
+};
 
+class Test {
+ public:
+  void goesWrong() {
+    throw MyException();
+  }
 };
 
 int main() {
+  Test test;
+
+  try {
+    test.goesWrong();
+  }
+  catch (MyException &e) {
+    cout << e.what() << endl;
+  }
   return 0;
 }
